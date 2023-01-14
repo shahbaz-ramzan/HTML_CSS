0045f7a5c53b45648096fd482b71dadf4d21a4b6
shahbazramzan <mshahbazramzan952@gmail.com>
2023-01-14 16:08:13 +0500

    jan14-updatet to do list

--- To do list/script.js
+++ To do list/script.js
@@ -1,8 +1,23 @@
-let myNodelist=document.getElementsByTagName("LI");
-for(let i =0;i<myNodelist.length;i++){
-    let span =document.createElement("span");
-    let txt = document.createTextNode("");
-    span.className="close";
-    span.appendChild(txt);
-    myNodelist[i].appendChildO(span);
+
+function newElement(){
+    let li =document.createElement("li");
+    let inputValue=document.getElementById("myInput").value ;
+    let t = document.createTextNode(inputValue);
+    let button =document.createElement("button");
+    let txt=document.createTextNode("delete")
+    button.appendChild(txt);
+
+    li.appendChild(t);
+    li.appendChild(button);
+    if(inputValue == ""){
+        alert("You must write something")
+    }
+    else
+    {
+        document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+
+    
+     
 }
