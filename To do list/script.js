d2a7d8e241584cbad64c582524b48bf368ad0ca2
shahbazramzan <mshahbazramzan952@gmail.com>
2023-01-16 12:27:28 +0500

    my to do lst

--- To do list/script.js
+++ To do list/script.js
@@ -1,23 +1,55 @@
-
-function newElement(){
-    let li =document.createElement("li");
-    let inputValue=document.getElementById("myInput").value ;
-    let t = document.createTextNode(inputValue);
-    let button =document.createElement("button");
-    let txt=document.createTextNode("delete")
-    button.appendChild(txt);
-
-    li.appendChild(t);
-    li.appendChild(button);
-    if(inputValue == ""){
-        alert("You must write something")
-    }
-    else
-    {
-        document.getElementById("myUL").appendChild(li);
-    }
-    document.getElementById("myInput").value = "";
-
-    
-     
+// Create a "close" button and append it to each list item
+let  myNodelist = document.getElementsByTagName("LI");
+let i;
+for (i = 0; i < myNodelist.length; i++) {
+  let button = document.createElement("button");
+  let txt = document.createTextNode("Delete");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(button);
 }
+
+// Click on a close button to hide the current list item
+let close = document.getElementsByClassName("close");
+
+for (let i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+let list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("myInput").value;
+  let t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  let span = document.createElement("button");
+  var txt = document.createTextNode("Delete");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(button);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      let div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+} 
---
+++ js-practice/script.js
@@ -0,0 +1,12 @@
+// add two number
+
+
+// function add(num1,num2){
+//     let sum = num1+num2;
+//     console.log(sum);
+// }
+// add(1,2);
+
+// Q2 swap two number without using 3rd variable
+
+
